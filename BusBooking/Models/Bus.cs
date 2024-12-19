4e6f315c5a417d8285f954bd8e1d9c53e22bc47f
Enes Kaan Boyar <202103001093@dogus.edu.tr>
2024-12-19 12:14:05 +0300

    initial commit with ASP.NET MVC project setup + Db configurations with backup file + Layout setup

---
+++ BusBooking/Models/Bus.cs
@@ -0,0 +1,14 @@
+﻿namespace BusBooking.Models
+{
+    public class Bus
+    {
+        public int BusId { get; set; }
+        public string BusNumber { get; set; }
+        public string DepartureLocation { get; set; }
+        public string ArrivalLocation { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public int CompanyId { get; set; }
+        public int SeatCount { get; set; }
+    }
+}
---
+++ BusBooking/Pages/Booking/Book.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using BusBooking.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BusBooking.Pages.Booking
+{
+    public class BookModel : PageModel
+    {
+        private readonly DatabaseHelper _databaseHelper;
+
+        public BookModel(DatabaseHelper databaseHelper)
+        {
+            _databaseHelper = databaseHelper;
+        }
+
+        [BindProperty]
+        public List<Bus> Buses { get; set; }
+
+        public void OnPost(string departureLocation, string arrivalLocation, DateTime date)
+        {
+            // Retrieve available Buses for spesific filters
+            Buses = _databaseHelper.RetrieveBusesForFilter(departureLocation, arrivalLocation, date);
+        }
+
+        public IActionResult OnPostBook(int busId, int seats)
+        {
+            // DB Helper handles booking on database for the bus id and seatcount.
+            bool success = _databaseHelper.HandleBooking(busId, seats);
+
+            if (success)
+            {
+                // Redirect to confirmation page
+                return RedirectToPage("BookingConfirmation");
+            }
+            else
+            {
+                // Show an error message
+                return RedirectToPage("Error");
+            }
+        }
+    }
+
+}
